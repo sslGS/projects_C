2b50137bcaa030ce82b81c4db059bae2f93dc8e8
gscs <gscs355@gmail.com>
2024-04-21 14:01:46 +0200

    Project

--- iop.cpp
+++ iop.cpp
@@ -1,30 +1,8 @@
 #include <iostream>
 /*Botwa sript*/
 typedef unsigned short bit;
-class Creature {
-   public:
-      Creature(bit health = 100) : m_Health(health){};
-      virtual void Greet() const{};
-      virtual void DisplayHealth() const {
-         std::cout << "Health " << m_Health << std::endl;
-      };
-   protected:
-      bit m_Health{};
-
-};
-
-class Orc : public Creature {
-   public:
-      Orc(bit health = 120) : Creature(health) {};
-      virtual void Greet() const {
-         std::cout << "The orc grunts hello\n";
-      };
-};
-
 
 auto main(int argc, char *argv[]) -> decltype(0) {
-   Creature *pCreature = new Orc();
-   pCreature->Greet();
-   pCreature->DisplayHealth();
+   
    return 0;
 }
